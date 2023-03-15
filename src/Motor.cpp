043419fa65302815a971038241a44352b5c1d12f
SimonPucheu <104694344+SimonPucheu@users.noreply.github.com>
2023-03-15 17:31:40 +0100

    Add move function & fix standby bug
    Add a move function: if the speed is positive, call the forward function. Otherwise, call the backward.
    Fix the standby bug: add a digitalWrite(standBy, HIGH) to take the motor off the standby mode

--- src/Motor.cpp
+++ src/Motor.cpp
@@ -15,6 +15,23 @@ Motor::Motor(uint8_t iIn1, uint8_t iIn2, uint8_t iPwm, uint8_t iStandBy, int iFr
 	in2 = iIn2;
 	pwm = iPwm;
 	standby = iStandBy;
+	pinMode(in1, OUTPUT);
+	pinMode(in2, OUTPUT);
+	pinMode(pwm, OUTPUT);
+	pinMode(standby, OUTPUT);
+	digitalWrite(standby, HIGH);
+}
+
+void Motor::move(int speed)
+{
+	if (speed > 0)
+	{
+		forward(speed);
+	}
+	if (speed < 0)
+	{
+		backward(-speed);
+	}
 }
 
 void Motor::forward(int speed)
